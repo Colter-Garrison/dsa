32f6ccf386b43ed87958442e6c214b655babfd4c
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-09-20 16:22:58 -0700

    stack tests passing

---
+++ day-02/tests/Stack.test.js
@@ -0,0 +1,22 @@
+const { Stack } = require('../Stack.js');
+
+describe('Stack', () => {
+  it('push should add an item to the top on an array', () => {
+    const newItemArr = new Stack([]);
+    newItemArr.classPush('fox');
+    newItemArr.classPush('goose');
+    newItemArr.classPush('lizard');
+    expect(newItemArr.readableArr).toEqual(['fox', 'goose', 'lizard']);
+  });
+
+  it('pop should remove the top item in the array', () => {
+    const newItemArr = new Stack(['fox', 'goose', 'lizard']);
+    newItemArr.classPop();
+    expect(newItemArr.readableArr).toEqual(['fox', 'goose']);
+  });
+
+  it('peek should return the last item in the array', () => {
+    const peekedItem = new Stack(['fox', 'goose', 'lizard']);
+    expect(peekedItem.classPeek()).toEqual('lizard');
+  });
+});
