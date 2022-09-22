bc104d960791ad769906dfd3976e8b2b3eac7b38
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-09-22 16:48:50 -0700

    first test passing

--- day-03/index.test.js
+++ day-03/index.test.js
@@ -1,5 +1,22 @@
+const { BinaryTreeNode } = require('./BinaryTreeNode');
+
 describe('node based data structures', () => {
   it('example test', () => {
     expect(1).toBe(1);
+  });
+
+  it('creates a new tree', () => { 
+    const A = new BinaryTreeNode('A');
+    const B = new BinaryTreeNode('B');
+    const C = new BinaryTreeNode('C');
+    const D = new BinaryTreeNode('D');
+
+    B.add(A);
+    B.add(C);
+    B.add(D);
+    expect(B.value).toEqual('B');
+    expect(B.left.value).toEqual('A');
+    expect(B.right.value).toEqual('C');
+    expect(B.right.right.value).toEqual('D');
   });
 });
